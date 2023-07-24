918a013bf36f5b79c28374f95725418cad1a7bd7
Jan Garwol <ziukowalski@gmail.com>
2023-07-24 16:06:06 +0200

    Add generated project

---
+++ src/main/java/com/jgarwol/dddreference/DddReferenceApplication.java
@@ -0,0 +1,13 @@
+package com.jgarwol.dddreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgarwol/dddreference/DddReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgarwol.dddreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
